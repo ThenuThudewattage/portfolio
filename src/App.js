bf392d615a804f02bc1ed3921007c876c44874eb
ThenuThudewattage <127599146+ThenuThudewattage@users.noreply.github.com>
2024-07-11 15:50:47 +0530

    navbar design completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import NavBar from './components/NavBar';
+import Banner from './components/Banner';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
     <div >
         <NavBar/>
+        <Banner/>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,14 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { useState, useEffect } from "react";
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
-export default function NavBar(){
-  const [activeLink, setActiveLink] = useState('home');
+
+export default function NavBar() {
+  const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -15,10 +20,10 @@ export default function NavBar(){
       } else {
         setScrolled(false);
       }
-    }
+    };
 
-    window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener('scroll', onScroll);
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
   const onUpdateActiveLink = (value) => {
@@ -29,7 +34,7 @@ export default function NavBar(){
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-          <img src={""} alt="logo" />
+          <img src={logo} alt="logo" />
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav">
@@ -69,13 +74,13 @@ export default function NavBar(){
           <span className="navbar-text">
             <div className="social-icon">
               <a href="#">
-                <img src={""} alt="" />
+                <img src={navIcon1} alt="" />
               </a>
               <a href="#">
-                <img src={""} alt="" />
+                <img src={navIcon2} alt="" />
               </a>
               <a href="#">
-                <img src={""} alt="" />
+                <img src={navIcon3} alt="" />
               </a>
             </div>
             <button className="vvd" onClick={() => console.log("connect")}>
@@ -87,4 +92,3 @@ export default function NavBar(){
     </Navbar>
   );
 }
-
