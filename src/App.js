8e5fb41546c4c6c799c4674be55bab0e2ffd3b9a
ThenuThudewattage <127599146+ThenuThudewattage@users.noreply.github.com>
2024-07-14 20:29:21 +0530

    added the footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Skills from './components/Skills';
+import Footer from './components/Footer';
 import {Projects} from './components/Projects';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -13,6 +14,7 @@ function App() {
         <Banner/>
         <Skills/>
         <Projects/>
+        <Footer/>
     </div>
   );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,27 +3,32 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Container, Row, Col } from "react-bootstrap";
 import colorSharp from "../assets/img/color-sharp.png";
+import reactLogo from "../assets/img/react-logo.png";
+import nodeLogo from "../assets/img/node-logo.png";
+import bootstrapLogo from "../assets/img/bootstrap-png.png";
+
+/* create several banners named people skills, and tech stacks and frameworks and tools */
 
 export default function Skills() {
-    const responsive = {
-      superLargeDesktop: {
-        // the naming can be any, depends on you.
-        breakpoint: { max: 4000, min: 3000 },
-        items: 5,
-      },
-      desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 3,
-      },
-      tablet: {
-        breakpoint: { max: 1024, min: 464 },
-        items: 2,
-      },
-      mobile: {
-        breakpoint: { max: 464, min: 0 },
-        items: 1,
-      },
-    };
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
     <section className="skill" id="skills">
       <Container>
@@ -42,15 +47,15 @@ export default function Skills() {
                   className="skill-sliderd"
                 >
                   <div className="item">
-                    <img src={""} alt="img" />
+                    <img src={reactLogo} alt="img" />
                     <h5>React</h5>
                   </div>
                   <div className="item">
-                    <img src={""} alt="img" />
+                    <img src={nodeLogo} alt="img" />
                     <h5>Node</h5>
                   </div>
                   <div className="item">
-                    <img src={""} alt="img" />
+                    <img src={bootstrapLogo} alt="img" />
                     <h5>Java</h5>
                   </div>
                   <div className="item">
@@ -67,7 +72,7 @@ export default function Skills() {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-left" src ={colorSharp}/>
+      <img className="background-image-left" src={colorSharp} />
     </section>
   );
 }
